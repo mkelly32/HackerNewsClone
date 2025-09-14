13169d7a761f4435721190bf3189f63ee2c4e167
Mike Kelly <mkelly32@tcd.ie>
2025-09-14 15:22:48 +0100

    move components into directory

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import { Header } from "./header";
-import { Body } from "./body";
+import { Header } from "./components/header";
+import { Body } from "./components/body";
 
 export const App: FC = () => {
   return (
